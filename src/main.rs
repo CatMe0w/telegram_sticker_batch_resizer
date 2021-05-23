7e936eca91ab97d60922e444e5d52bd8df77735c
CatMe0w <catme0w@live.cn>
2021-05-24 00:06:37 +0800

    Progress bar & graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
-use std::env;
+use std::{env, process};
+use std::io::{stdin, stdout, Read, Write};
 use image::GenericImageView;
 use image::imageops::FilterType;
+use indicatif::ProgressBar;
+
+fn pseudo_pause() {
+    let mut stdout = stdout();
+    stdout.write(b"\nPress [Enter] to exit...").unwrap();
+    stdout.flush().unwrap();
+    stdin().read(&mut [0]).unwrap();
+}
+
+fn graceful_shutdown(code: i32) {
+    pseudo_pause();
+    process::exit(code);
+}
 
 fn main() {
     // iterate args into image processing logic
     let args: Vec<String> = env::args().collect();
     if let None = args.get(1) {
-        panic!("Hey! Drag some pictures into me to resize!");
+        println!("Hey! Drag some pictures into me to resize!");
+        graceful_shutdown(1);
     }
 
+    let bar = ProgressBar::new(args.len() as u64 - 1);
+
+    let mut too_small_exist = false;
+
     for i in &args[1..] {
+        bar.inc(1);
+
         // die while encountering unsupported formats
         let img = image::open(&i).unwrap();
         let width = img.dimensions().0;
@@ -17,6 +38,7 @@ fn main() {
 
         // determine whether pics are too small and inform the user
         if width < 512 && height < 512 {
+            too_small_exist = true;
             println!("This image \"{}\" seems to be too small. A poor result may be expected.", i);
         }
 
@@ -40,6 +62,13 @@ fn main() {
             }
             _ => ()
         }
-        img.save(i.to_owned() + ".resized.png").unwrap();
+        img.save(format!("{}.resized.png", i)).unwrap();
     }
+
+    bar.finish();
+    println!("All done! Have fun!");
+    if too_small_exist == true {
+        println!("HINT: You can use upscaling tools such as waifu2x on your pictures before drag into resizer.");
+    }
+    graceful_shutdown(0);
 }
